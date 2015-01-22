69f18a018acb4dc411efd47cd69490674b3f1425
Ronald Chan <ronalchn@gmail.com>
2015-01-23 03:10:03 +1300

    remove db migrate adding from engine

--- lib/nzic_models/engine.rb
+++ lib/nzic_models/engine.rb
@@ -3,14 +3,6 @@ module NZICModels
 
     config.autoload_paths << File.expand_path("../../app/models", __FILE__)
 
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths["db/migrate"].expanded.each do |expanded_path|
-          app.config.paths["db/migrate"] << expanded_path
-        end
-      end
-    end
-
     #initializer :model_autoload, before: :set_autoload_paths do |app|
 #puts %W(#{root}/app/models)
 #puts ActiveService::Configuration.path
